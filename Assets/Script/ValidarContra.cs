b6be9c636ee8c4336e602374d34c410df8bdc3d4
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-04-22 22:53:53 -0300

    Video 5

--- Assets/Script/ValidarContra.cs
+++ Assets/Script/ValidarContra.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContra : MonoBehaviour
 {
+    string contraCorrecta;
+    string contraUsuario;
+    public Text ingresoUsuario;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -18,7 +23,15 @@ public class ValidarContra : MonoBehaviour
 
     public void validarContra()
     {
-
+        contraUsuario = ingresoUsuario.text;
+        if (contraUsuario == contraCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 
 }
